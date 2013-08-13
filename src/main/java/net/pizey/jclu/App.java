6f3e6656aaee1bff44ac6374b26b5d15385cb9a1
timp21337 <timp@paneris.org>
2013-08-13 16:11:59 +0100

    Generated by maven, within eclipse

---
+++ src/main/java/net/pizey/jclu/App.java
@@ -0,0 +1,13 @@
+package net.pizey.jclu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
