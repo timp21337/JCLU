da300043c08dc6dc9396d193822fbbd54eb5f82a
timp21337 <timp@paneris.org>
2013-08-14 17:02:04 +0100

    Javadoc

--- src/test/java/net/pizey/test/rdd/package-info.java
+++ src/test/java/net/pizey/test/rdd/package-info.java
@@ -1,5 +1,10 @@
 /**
+ * The idea is that a dated Requirements Verification Document 
+ * will be produced by running the tests and that the tests 
+ * will fail if there is an unmet requirement. 
+ * 
  * @author timp
- *
+ * @since 2013-08-13
+ * @see http://tim-pizey.blogspot.co.uk/2013/08/reimplementing-length-unit-converter-in.html
  */
 package net.pizey.test.rdd;
