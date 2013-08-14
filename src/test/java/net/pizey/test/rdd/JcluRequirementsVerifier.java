ef86a80eea57ea2ad518fcbe7607c970c7fa7be4
timp21337 <timp@paneris.org>
2013-08-14 17:03:11 +0100

    Refactor as junit tests, remove main method

--- src/test/java/net/pizey/test/rdd/JcluRequirementsVerifier.java
+++ src/test/java/net/pizey/test/rdd/JcluRequirementsVerifier.java
@@ -1,27 +1,44 @@
 package net.pizey.test.rdd;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 
 import net.pizey.jclu.AppTest;
 
+/**
+ * @author timp
+ * @since 2013-08-13
+ */
 public class JcluRequirementsVerifier extends RequirementsVerifier {
 
-  public static void addRequirements() {
-    addRequirement("01", "foo");
-    addRequirement("02", "bar");
+  public void addRequirements() {
+    addRequirement("01", "Conversions must be reflexive.");
+    addRequirement("02", "Conversions must be symetric.");
+    /*
+    addRequirement("03", "Conversions must be transitive.");
+    addRequirement("04", "Expressions may only be composed of units " + 
+                         "where subsequent units are smaller than preceding.");
+    addRequirement("05", "All length units must be convertible to each other.");
+    addRequirement("06", "Rounding errors must not fail comparisons.");
+    addRequirement("07", "There must be a mechanism to add new units.");
+    addRequirement("08", "An object must be creatable from a string representation.");
+    addRequirement("09", "Anticipated exceptions must be tested.");
+    addRequirement("10", "Lengths must have an additional unabbreviated textual description.");
+*/
+  }
+  
+  @Override
+  public void addTestClasses() {
+    tests.add(AppTest.class);    
   }
 
-  private static void addRequirement(String ref, String title) {
-    requirements.put(ref, title);
-    requirementTests.put(ref, new ArrayList<String>());
-  }
-
-  public static void main(String[] args) {
-    addRequirements();
-    Class<?>[] tests = new Class<?>[] { AppTest.class };
-    for (Class<?> t : tests)
-      readAnnotation(t);
-    printRequirementVerificationMatrix();
+  public void testAllRequirements() throws Exception { 
+    File dir = new File("qa/RequirementsVerification");
+    dir.mkdirs();
+    PrintStream out = new PrintStream(
+        new FileOutputStream("qa/RequirementsVerification/index.html")); 
+    assertFalse("Some Requirements still unaddressed", !doIt(out));    
   }
 
 }
--- src/test/java/net/pizey/test/rdd/RequirementsVerifier.java
+++ src/test/java/net/pizey/test/rdd/RequirementsVerifier.java
@@ -1,63 +1,85 @@
 package net.pizey.test.rdd;
 
+import java.io.PrintStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.HashMap;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
-public abstract class RequirementsVerifier {
+import junit.framework.TestCase;
+
+/**
+ * Enable the cross referencing of tests to requirements.
+ * 
+ * @author timp
+ * @since 2013-08-13
+ */
+public abstract class RequirementsVerifier extends TestCase {
 
   static SortedMap<String, String> requirements = new TreeMap<String, String>();
   static SortedMap<String, ArrayList<String>> requirementTests = new TreeMap<String, ArrayList<String>>();
+  static ArrayList<Class<?>> tests = new ArrayList<Class<?>>();
   
+  static void addRequirement(String ref, String title) {
+    requirements.put(ref, title);
+    requirementTests.put(ref, new ArrayList<String>());
+  }
+
   static void readAnnotation(Class<?> test) {
-    System.out.println("Annotation element for " + test.getSimpleName());
     for (Method method : test.getMethods()) {
       if (method.isAnnotationPresent(Requirements.class)) {
-        System.out.println(method.getName());
-        // getAnnotation returns Annotation type
         Annotation singleAnnotation = method.getAnnotation(Requirements.class);
-        Requirements requirementRefs = (Requirements) singleAnnotation;
-
+        Requirements requirementRefs = (Requirements)singleAnnotation;
         for (String id : requirementRefs.value()){
-          System.out.println(" " + id + ", " + requirements.get(id));
           requirementTests.get(id).add(test.getSimpleName() + "." + method.getName());
         }
-        System.out.print("\n");
       }
     }
   }
   
-  static void printRequirementVerificationMatrix() { 
-    System.out.println("<html>");
-    System.out.println("<body>");
-    System.out.println("<h1>Requirements Verification Matrix</h1>");
-    System.out.print("<h2>");
+  static boolean printRequirementVerificationMatrix(PrintStream out) { 
+    boolean allAddressed = true;
+    out.println("<html>");
+    out.println("<body>");
+    out.println("<h1>Requirements Verification Matrix</h1>");
+    out.print("<h2>");
     DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
     Calendar cal = Calendar.getInstance();
-    System.out.print(dateFormat.format(cal.getTime()));
-    System.out.println("</h2>");
+    out.print(dateFormat.format(cal.getTime()));
+    out.println("</h2>");
     
     for (String ref : requirements.keySet()){
-      System.out.println("<h3>" + ref + " " + requirements.get(ref) + "</h3>");
-      System.out.println("<ul>");
+      out.println("<h3>" + ref + " " + requirements.get(ref) + "</h3>");
+      out.println("<ul>");
       boolean hadOne = false;
       for (String test : requirementTests.get(ref)){
-        System.out.println(" <li>" + test + "</li>");
+        out.println(" <li>" + test + "</li>");
         hadOne = true;
       }
-      if (!hadOne)
-        System.out.println(" <li><font color='red'>None</font></li>");
-      System.out.println("</ul>");
+      if (!hadOne){ 
+        out.println(" <li><font color='red'>None</font></li>");
+        allAddressed = false;
+      }
+      out.println("</ul>");
     }
     
-    System.out.println("</body>");
-    System.out.println("</html>");
+    out.println("</body>");
+    out.println("</html>");
+    return allAddressed;
   }
 
+  public boolean doIt(PrintStream out) {
+    addRequirements();
+    addTestClasses();
+    for (Class<?> t : tests)
+      readAnnotation(t);
+    return printRequirementVerificationMatrix(out);
+  }
+
+  public abstract void addRequirements();
+  public abstract void addTestClasses();
 }
