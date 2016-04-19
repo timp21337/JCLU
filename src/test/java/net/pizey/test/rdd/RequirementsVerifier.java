a52e530d7ff0e15148791f705a1444ac3907c680
Tim Pizey <pizeyt@elsevier.com>
2016-04-20 00:02:28 +0100

    HTML functions

--- src/test/java/net/pizey/test/rdd/RequirementsVerifier.java
+++ src/test/java/net/pizey/test/rdd/RequirementsVerifier.java
@@ -40,35 +40,70 @@ public abstract class RequirementsVerifier extends TestCase {
       }
     }
   }
-  
-  static boolean printRequirementVerificationMatrix(PrintStream out) { 
+
+  static String otag(String tag) {
+    return "<" + tag + ">" + "\n";
+  }
+  static String ctag(String tag) {
+    return "</" + tag + ">" + "\n";
+  }
+  static String tag(String tag, String contents) {
+    return otag(tag)
+        + contents + "\n"
+        + ctag(tag);
+  }
+  static String html(String s) {
+    return tag("head", s);
+  }
+  static String head(String s) {
+    return tag("head", s);
+  }
+  static String title(String s) {
+    return tag("title", s);
+  }
+  static String body(String s) {
+    return tag("body", s);
+  }
+  static String h1(String s) {
+    return tag("h1", s);
+  }
+  static String h2(String s) {
+    return tag("h2", s);
+  }
+  static String h3(String s) {
+    return tag("h3", s);
+  }
+  static String li(String s) {
+    return tag("li", s);
+  }
+  static boolean printRequirementVerificationMatrix(PrintStream out) {
     boolean allAddressed = true;
-    out.println("<html>");
-    out.println("<body>");
-    out.println("<h1>Requirements Verification Matrix</h1>");
-    out.print("<h2>");
+    out.println(otag("html")
+        + head(
+        title("Requirements Verification Matrix")
+        + "<link rel=\"stylesheet\" type=\"text/css\" href=\"rdd.css\">\n")
+        + otag("body"));
+    out.println(h1("RDD - Requirements Verification Matrix"));
     DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
     Calendar cal = Calendar.getInstance();
-    out.print(dateFormat.format(cal.getTime()));
-    out.println("</h2>");
-    
+    out.print(h2(dateFormat.format(cal.getTime())));
+
     for (String ref : requirements.keySet()){
       out.println("<h3>" + ref + " " + requirements.get(ref) + "</h3>");
       out.println("<ul>");
       boolean hadOne = false;
       for (String test : requirementTests.get(ref)){
-        out.println(" <li>" + test + "</li>");
+        out.println(li(test));
         hadOne = true;
       }
       if (!hadOne){ 
-        out.println(" <li><font color='red'>None</font></li>");
+        out.println(li("<font color='red'>None</font>"));
         allAddressed = false;
       }
       out.println("</ul>");
     }
     
-    out.println("</body>");
-    out.println("</html>");
+    out.println(ctag("body") + ctag("html"));
     return allAddressed;
   }
 
